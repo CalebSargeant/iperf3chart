1f66afb73c6cc41cfbbffc43d32b996a95c91f4a
Caleb Sargeant <calebsargeant@gmail.com>
2020-03-10 15:26:38 +0200

    Added custom port capability

--- bw-test.sh
+++ bw-test.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 # Run iperf3 -s on target
 
+blank() { [[ -z $1 ]]; }
+
+if blank "$2"; then
+  port="-p 5201"
+else
+  port="-p $2"
+fi
+
+
 target=${1:-nuc.local}
 dir=$target/$(date -u +%Y-%m-%d)/$(ip route get 8.8.8.8 2>/dev/null| awk '{print $5}')
 mkdir -p "$dir" || true
-iperf3 -J -c "$target" > "$dir"/"$(date +%s)".json
+iperf3 -J -c "$target" "$port" > "$dir"/"$(date +%s)".json
